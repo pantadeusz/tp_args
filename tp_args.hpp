5cfa685542ae537964aca3392431afc3a5c910f9
pantadeusz <none@none.com>
2022-05-09 21:11:03 +0200

    format

--- tp_args.hpp
+++ tp_args.hpp
@@ -7,13 +7,13 @@
  *
  * @copyright Copyright (c) 2022
  *
- * This library allows for simple parsing arguments in the format: -argname argvalue. 
- * 
+ * This library allows for simple parsing arguments in the format: -argname argvalue.
+ *
  * @code {.c++ }
  * auto help = arg(argc, argv, "help", false);
  * auto x = arg(argc, argv,"x",10.0, "the x value");
  * @endcode
- * 
+ *
  */
 #ifndef __TP_ARGS_HPP____
 #define __TP_ARGS_HPP____
@@ -29,35 +29,34 @@
  *
  */
 
-namespace tp::args
-{
-std::map<std::string,std::string> known_args;
+namespace tp::args {
+std::map<std::string, std::string> known_args;
 
 template<typename T>
-T arg (int argc, char** argv, std::string name, T default_value, std::string description = "") {
-    using namespace std;
-    any ret = default_value;
-    for (int i = 0; i < argc; i++) {
-        if ((std::string(argv[i]).substr(1) == name) && (argv[i][0] == '-')) {
-			std::string argument = (i<(argc-1))?argv[i+1]:"1";
-            if (std::is_same_v<T, bool>) { 
-				ret = (argument=="true") || (argument=="1") || (argument=="yes") || ((argument.size() > 0) && (argument[0] == '-'));
+T arg ( int argc, char** argv, std::string name, T default_value, std::string description = "" ) {
+	using namespace std;
+	any ret = default_value;
+	for ( int i = 0; i < argc; i++ ) {
+		if ( ( std::string( argv[i] ).substr( 1 ) == name ) && ( argv[i][0] == '-' ) ) {
+			std::string argument = ( i < ( argc - 1 ) ) ? argv[i + 1] : "1";
+			if ( std::is_same_v<T, bool> ) {
+				ret = ( argument == "true" ) || ( argument == "1" ) || ( argument == "yes" ) || ( ( argument.size() > 0 ) && ( argument[0] == '-' ) );
 			} else {
-				if (std::is_same_v<T, int>) ret = stoi(argument);
-				else if (std::is_same_v<T, double>) ret = stod( argument );
-				else if (std::is_same_v<T, unsigned>) ret = (unsigned)stoul( argument );
-				else if (std::is_same_v<T, unsigned long>) ret = stoul( argument );
-				else if (std::is_same_v<T, char>) ret = argument.at(0);
+				if ( std::is_same_v<T, int> ) ret = stoi( argument );
+				else if ( std::is_same_v<T, double> ) ret = stod( argument );
+				else if ( std::is_same_v<T, unsigned> ) ret = ( unsigned )stoul( argument );
+				else if ( std::is_same_v<T, unsigned long> ) ret = stoul( argument );
+				else if ( std::is_same_v<T, char> ) ret = argument.at( 0 );
 				else ret = argument;
 			}
-        }
-    }
-    known_args[name] = description;
-    return std::any_cast<T>(ret);
+		}
+	}
+	known_args[name] = description;
+	return std::any_cast<T>( ret );
 };
 
-auto args_info = [](auto &o) {
-	for (auto [k,v] : known_args) {
+auto args_info = []( auto &o ) {
+	for ( auto [k, v] : known_args ) {
 		o << " -" << k << "\t" << v << "\n";
 	}
 };
